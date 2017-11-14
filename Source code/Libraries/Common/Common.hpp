abc4f0dd93a4f0b62f81a2f795a4f9973281ceaf
TheGreatV <GreatV@email.ua>
2017-11-14 13:39:49 +0200

    [New] Libraries/Common source code for Visual Studio 2013

---
+++ Source code/Libraries/Common/Common.hpp
@@ -0,0 +1,24 @@
+#pragma region Include
+
+#pragma once
+
+#pragma endregion
+
+
+#pragma region Macros
+#pragma endregion
+
+
+namespace GreatVEngine
+{
+}
+
+
+#pragma region GreatVEngine
+#pragma endregion
+
+
+#pragma region
+#pragma endregion
+
+
