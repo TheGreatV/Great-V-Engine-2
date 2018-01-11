c55635ac3bfe53396d92d3226a147a63fe89367b
TheGreatV <GreatV@email.ua>
2018-01-12 01:27:01 +0200

    [New] Draft version of Graphics Library

---
+++ Source code/Libraries/Graphics/Graphics.hpp
@@ -0,0 +1,35 @@
+#pragma region Include
+
+#pragma once
+
+#include "Header.hpp"
+
+#pragma endregion
+
+
+#pragma region Macros
+
+#pragma endregion
+
+
+namespace GreatVEngine2
+{
+	namespace Graphics
+	{
+	}
+}
+
+
+#pragma region GreatVEngine2
+
+#pragma region Graphics
+
+#pragma endregion
+
+#pragma endregion
+
+
+#pragma region
+#pragma endregion
+
+
---
+++ Source code/Libraries/Graphics/Header.hpp
@@ -0,0 +1,33 @@
+#pragma region Include
+
+#pragma once
+
+#pragma endregion
+
+
+#pragma region Macros
+
+#pragma endregion
+
+
+namespace GreatVEngine2
+{
+	namespace Graphics
+	{
+	}
+}
+
+
+#pragma region GreatVEngine2
+
+#pragma region Graphics
+
+#pragma endregion
+
+#pragma endregion
+
+
+#pragma region
+#pragma endregion
+
+
